4da35e2a50d89315f095d55ec08753fe834200fa
fineiasbitu <fineiasbituu@gmail.com>
2022-06-24 11:56:08 -0300

    Corrige registros no banco de dados

--- tcc python/cria.py
+++ tcc python/cria.py
@@ -5,12 +5,12 @@ con = sqlite3.connect(nome_banco)
 cur = con.cursor()
 # id INTEGER, modelo TEXT, cor TEXT, ano INTEGER, preco REAL, automatico INTEGER
 produtos_loja = [
-    (None, 'vitoria secret', 'bolsa transversal', 'branca', 'm', 2, 1.700),
-    (None, 'louis vuitton', 'mochila de costa', 'marrom', 'p', 3, 3.50),
-    (None, 'victor hugo', 'transversal corrente', 'preta', 'g', 5, 600),
-    (None, 'Louis Vuitton', 'Bolsa tiracolo', 'preta', 'm', 5, 600),
-    (None, 'Gucci', 'Bolsa transversal', 'marrom', 'g', 5, 600),
-    (None, 'Bolsa Michael Kors', 'transversal', 'preta', 'p', 5, 600)
+    (None, 'Vitoria Secret', 'Bolsa transversal', 'Branca', 'm', 2, 1700.0),
+    (None, 'Louis Vuitton', 'Mochila de costa', 'Marrom', 'p', 3, 3500.0),
+    (None, 'Victor Hugo', 'Transversal corrente', 'Preta', 'g', 5, 1300.0),
+    (None, 'Louis Vuitton', 'Bolsa tiracolo', 'Preta', 'm', 5, 900.0),
+    (None, 'Gucci', 'Bolsa transversal', 'Marrom', 'g', 4, 2500.0),
+    (None, 'Bolsa Michael Kors', 'Transversal', 'Preta', 'p', 2, 1100.0)
 
 ]
 
